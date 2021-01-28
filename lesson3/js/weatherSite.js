da48058678480a62b89a3b92831699bd5df85896
Use your GitHub username <Enter your email address>
2021-01-27 20:55:58 -0700

    validation errors

--- lesson3/js/weatherSite.js
+++ lesson3/js/weatherSite.js
@@ -2,3 +2,8 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 
 var x = document.lastModified;
 document.getElementById("date").innerHTML = x;
+
+function toggleMenu() {
+   
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
