5c104f58acda915d45d46355ccedb847012b5f42
Use your GitHub username <Enter your email address>
2021-02-11 22:11:37 -0700

    lesson6

---
+++ lesson6/js/prestonPage.js
@@ -0,0 +1,13 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+function lastModified() {
+    var d = new Date("")
+}
+
+
+
+
+function toggleMenu() {
+   
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
