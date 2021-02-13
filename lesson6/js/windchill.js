e8c322228384f6c9f74ed8081344b459b7edd993
Use your GitHub username <Enter your email address>
2021-02-12 21:08:37 -0700

    speed

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -6,7 +6,7 @@
     var f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16);
  
       if (temp <= 50 && speed >3) {
-         f = f.toFixed(0);
+         f = f.toFixed(1);
       }  
 else {
     f = "N/A";
