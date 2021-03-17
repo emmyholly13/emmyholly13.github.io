01b3dfcbb21aeef82c1cc73980e24f941762db42
Use your GitHub username <Enter your email address>
2021-03-16 22:50:46 -0600

    forecast

--- lesson-10/js/forecast.js
+++ lesson-10/js/forecast.js
@@ -1,15 +1,28 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=7f0d84b192d7b06436b826401d169039";
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=7f0d84b192d7b06436b826401d169039";
 
-fetch(apiURL)
+fetch(forecastURL)
 .then((response) => response.json())
 .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
 
+    
+    const forecast = jsObject["list"];
+    const forecastfilter = forecast.filter((x) => x.dt_txt.includes("18:00:00"));
+    console.log(forecastfilter);
+
+
+
+    
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; //note the concatenation
-    const desc = jsObject.weather[0].description; //not how we reference the weather array
+    const desc = jsObject.weather[0].description; //note how we reference the weather array
     document.getElementById('imagesrc').textContent = imagesrc; //informational specification only
     document.getElementById('icon').setAttribute('src', imagesrc);
     document.getElementById('icon').setAttribute('alt', desc);
+
+
+
+
+
+
 
 });
--- lesson-10/js/prestonweatherapi.js
+++ lesson-10/js/prestonweatherapi.js
@@ -5,17 +5,8 @@ fetch(apiURL)
 .then((jsObject) => {
     console.log(jsObject);
     document.getElementById('current-temp').textContent = jsObject.main.temp;
-
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; //note the concatenation
-    const desc = jsObject.weather[0].description; //not how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc; //informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);
-    document.getElementById('icon').setAttribute('alt', desc);
-
-
-
-
-
-
+    document.getElementById('high-temp').textContent = jsObject.main.temp_max;
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('speed').textContent = jsObject.wind.speed;
 
 });
