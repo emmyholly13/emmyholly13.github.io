1b1a938d51126e86ce41304e73c7e81568395b57
Use your GitHub username <Enter your email address>
2021-03-17 22:27:32 -0600

    img

--- lesson-10/js/forecast.js
+++ lesson-10/js/forecast.js
@@ -1,37 +1,35 @@
 const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=7f0d84b192d7b06436b826401d169039";
 
 fetch(forecastURL)
-    .then((response) => response.json())
-    .then((forecastjsObject) => {
-        console.log(forecastjsObject);
+  .then((response) => response.json())
+  .then((forecastjsObject) => {
+    console.log(forecastjsObject);
 
 
-        const forecast = forecastjsObject["list"];
-        const table = document.querySelector("#fiveday");
+    const forecast = forecastjsObject["list"];
+    const table = document.querySelector("#fiveday");
 
-        const forecastfilter = forecast.filter((x) => x.dt_txt.includes("18:00:00"));
-        console.log(forecastfilter);
+    const forecastfilter = forecast.filter((x) => x.dt_txt.includes("18:00:00"));
+    console.log(forecastfilter);
 
-        forecastfilter.forEach((filtered) => {
-          let tableimg = document.createElement('img');
-          let tabledata = document.createElement('td');
-
-
-
-
-            let tableimgURL = "https://openweathermap.org/img/w/" + filtered.weather[0].icon + ".png";
-            tableimg.setAttribute("src", tableimgURL);
-            tableimg.setAttribute("alt", filtered.weather[0].description);
-            tabledata.innerHTML = filtered.main.temp.toFixed(0);
+    forecastfilter.forEach((filtered) => {
+      let tabledata = document.createElement('td');
+      let tableimg = document.createElement('img');
+      
 
 
-            console.log(tableimgURL);
-table.appendChild(tabledata);
-          
-
-        });
 
 
+      let tableimgURL = "https://openweathermap.org/img/w/" + filtered.weather[0].icon + ".png";
+      tableimg.setAttribute("src", tableimgURL);
+      tableimg.setAttribute("alt", filtered.weather[0].description);
+      tabledata.innerHTML = filtered.main.temp.toFixed(0);
+
+
+      console.log(tableimgURL);
+      table.appendChild(tabledata);
 
 
     });
+
+  });
