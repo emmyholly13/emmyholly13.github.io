384d5b4cedd1bdc37be458d872bf764e71877532
Use your GitHub username <Enter your email address>
2021-03-17 20:44:50 -0600

    forecast table

--- lesson-10/js/forecast.js
+++ lesson-10/js/forecast.js
@@ -1,32 +1,37 @@
 const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=7f0d84b192d7b06436b826401d169039";
 
 fetch(forecastURL)
-.then((response) => response.json())
-.then((forecastjsObject) => {
-    console.log(forecastjsObject);
-
-    
-    const forecast = forecastjsObject["list"];
-const table =document.querySelector("fiveday");
-
-    const forecastfilter = forecast.filter((x) => x.dt_txt.includes("18:00:00"));
-    console.log(forecastfilter);
+    .then((response) => response.json())
+    .then((forecastjsObject) => {
+        console.log(forecastjsObject);
 
 
+        const forecast = forecastjsObject["list"];
+        const table = document.querySelector("fiveday");
 
+        const forecastfilter = forecast.filter((x) => x.dt_txt.includes("18:00:00"));
+        console.log(forecastfilter);
 
-
-    
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; //note the concatenation
-    const desc = jsObject.weather[0].description; //note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc; //informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);
-    document.getElementById('icon').setAttribute('alt', desc);
-
-document.querySelector('#fiveday').appendchild(day);
+        forecastfilter.forEach((filtered) => {
+          let tableimg = document.createElement('img');
+          let tabledata = document.createElement('td');
 
 
 
 
+            let tableimgURL = "https://openweathermap.org/img/w/" + '${filtered.weather[0].icon}' + ".png";
+            tableimg.setAttribute("src", tableimgURL);
+            tableimg.setAttribute("alt", '${filtered.weather[0].description}');
+            tabledata.innerHTML = '${filtered.main.temp.toFixed(0)}&deg;F';
 
-});
+
+            console.log(tableimgURL);
+
+            table.appendChild(tabledata);
+
+        });
+
+
+
+
+    });
